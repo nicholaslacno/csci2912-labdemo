44f8cf5ec40b34c53d18656f110c02aff63cab05
Nicholas Lacno <nlacno1@my.hpu.edu>
2020-11-07 13:52:11 -1000

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package pkg2912assignments;
+/**
+ * 
+ * @author Nicholas Lacno
+ * @date 11/7/2020
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
