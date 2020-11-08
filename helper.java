8a09cb9cef54afee9241f951b00339f9f8f38d32
Nicholas Lacno <nlacno1@my.hpu.edu>
2020-11-07 14:35:49 -1000

    helpInital commit

---
+++ helper.java
@@ -0,0 +1,11 @@
+/**
+ * @author Nicholas Lacno
+ * @date 11/7/2020
+ */
+public class helper{
+
+    public static void main(String[] args){
+        System.out.println("A Helpful Message");
+    }
+
+}
