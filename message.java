f9ed2de4b397407cc3371c5fcf9dc769089deb5b
Nicholas Lacno <nlacno1@my.hpu.edu>
2020-11-07 15:04:19 -1000

    Inital commit

---
+++ message.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Nicholas Lacno
+ * @date 11/7/2020
+ */
+public class message {
+    
+    public static void main(String[] args) {
+        Hello.greeting("Sup");
+        
+    }
+    
+}
